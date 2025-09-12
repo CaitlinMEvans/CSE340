08eac61ece8577a3c3fc2a84f5c03971c6a51939
Caitlin Evans <caitmsevans@gmail.com>
2025-09-12 10:05:14 -0600

    week3 activity 3 - Robust Error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/")   // <-- add this
+const utilities = require("./utilities/")  
 
 /* ***********************
  * View Engine and Templates
@@ -23,7 +23,8 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 
 // Index route
-app.get("/", baseController.buildHome)
+// app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
@@ -54,21 +55,30 @@ app.use(async (err, req, res, next) => {
   console.error(`Error at "${req.originalUrl}": ${err.message}`)
   const nav = await utilities.getNav()
   const status = err.status || 500
+  const message =
+    status === 404
+      ? err.message
+      : `Flux capacitor misfire. Our timeline diverged.<br><a href="/">Return to 1985 (Home)</a>.`
+
   res
     .status(status)
     .render("errors/error", {
       title: status === 404 ? "Great Scott! 404" : "Uh-oh! 500",
-      message: err.message || "Something went sideways. Flux capacitor not included.",
+      message,
       nav
     })
 })
 
 /* ***********************
  * Local Server Information
+ * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
 
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
